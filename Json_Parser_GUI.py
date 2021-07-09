f9df16a7edcf6c2c1ee40083c7d3d8060455f917
seokjun jin <seokjun.jin@devsisters.com>
2021-07-09 16:09:38 +0900

    파일명 읽어오기
    Json 파일명 읽어와 동일한 이름의 CSV 파일로 동일 경로에 남기기

--- Json_Parser_GUI.py
+++ Json_Parser_GUI.py
@@ -1,27 +1,27 @@
-import tkinter
+import tkinter as tk
 import tkinter.filedialog as fd
+import json
+import pandas as pd
 
-window=tkinter.Tk()
+def browsefunc():
+    file = fd.askopenfile()
+    if file:
+        print(file.name) # 파일을 제대로 읽어왔는지 확인
+    contents = file.read()
+    json_data = json.loads(contents)
+    df = pd.json_normalize(json_data["records"])
+    df.to_csv('sample+1.csv')
+
+window=tk.Tk()
 
 window.title("Json Converter")
 window.geometry("640x440+100+100")
 window.resizable(True, True)
 
-label=tkinter.Label(window,text="Json 파일을 CSV 파일로 변경합니다")
+label=tk.Label(window,text="Json 파일을 CSV 파일로 변경합니다")
 label.pack()
 
-button = tkinter.Button(window, overrelief="solid", width=10, text="파일 열기")
+button = tk.Button(window, overrelief="solid", width=10, text="파일 열기", command=browsefunc)
 button.pack()
-
-
-def browsefunc():
-    filename = fd.askopenfilename()
-    pathlabel.config(text=filename)
-
-browsebutton = button(window, text="파일 열기", command=browsefunc())
-browsebutton.pack()
-
-printlabel = Label(window)
-pathlabel.pack()
 
 window.mainloop()
--- Json_converter.py
+++ Json_converter.py
@@ -5,4 +5,3 @@ with open('tube_01.json', "r", encoding="utf8") as f:
     contents = f.read()
     json_data = json.loads(contents)
     df = pd.json_normalize(json_data["records"])
-    df.to_csv('sample.csv')
